044b56df611d7f4072d73bab5300b6b66a8cdf66
RazvanCristea <razvancristea1994@gmail.com>
2021-05-09 17:42:34 +0300

    Movie Entity

---
+++ MultimediaCenter/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultimediaCenter.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } 
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public MovieGenre Genre { get; set; }
+        
+        [MinLength(5)]
+        public int Duration { get; set; }
+        [MinLength(1990)]
+        public int YearOfRelease { get; set; }
+        [Required]
+        public string Director { get; set; }
+        public DateTime AddedDate { get; set; }
+        [Range(1, 10)]
+        public int Rating { get; set; }
+        public bool Watched { get; set; }
+    }
+}
